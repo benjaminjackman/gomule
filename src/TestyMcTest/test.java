f6ddc129166e34cc3215c9f703494a305574688c
Ben Jackman <ben@jackman.biz>
2016-01-09 11:54:09 -0600

    got project to compile

--- src/TestyMcTest/test.java
+++ src/TestyMcTest/test.java
@@ -3,24 +3,23 @@ package TestyMcTest;
 import org.apache.xmlrpc.webserver.ServletWebServer;
 import org.apache.xmlrpc.webserver.XmlRpcServlet;
 
-public class test{
+public class test {
 
-	private final int port = 8080;
-	
-	public test()  throws Exception{
+    private final int port = 8080;
 
-	          XmlRpcServlet servlet = new XmlRpcServlet();
-	          ServletWebServer webServer = new ServletWebServer(servlet, port);
-	          webServer.start();
-	      }
+    public test() throws Exception {
+//
+//        XmlRpcServlet servlet = new XmlRpcServlet();
+//        ServletWebServer webServer = new ServletWebServer(servlet, port);
+//        webServer.start();
+    }
 
-	
-	
-	public static void main(String[] args) throws Exception {
 
-		new test();
-		
-		
-	}
+    public static void main(String[] args) throws Exception {
+
+        new test();
+
+
+    }
 
 }
--- src/gomule/dropCalc/remote/DCXmlRpcServer.java
+++ src/gomule/dropCalc/remote/DCXmlRpcServer.java
@@ -6,28 +6,26 @@ import org.apache.xmlrpc.webserver.ServletWebServer;
 import org.apache.xmlrpc.webserver.XmlRpcServlet;
 
 
+public class DCXmlRpcServer {
 
-public class DCXmlRpcServer{
+    private final int port = 8080;
+    public static final DCNew DC = new DCNew();
 
-	private final int port = 8080;
-	public static final DCNew DC = new DCNew();
-	
-	
-	public DCXmlRpcServer()  throws Exception{
 
-	          XmlRpcServlet servlet = new XmlRpcServlet();
-	          ServletWebServer webServer = new ServletWebServer(servlet, port);
-	          
-	          webServer.start();
-	      }
+    public DCXmlRpcServer() throws Exception {
 
-	
-	
-	public static void main(String[] args) throws Exception {
+//        XmlRpcServlet servlet = new XmlRpcServlet();
+//        ServletWebServer webServer = new ServletWebServer(servlet, port);
+//
+//        webServer.start();
+    }
 
-		new DCXmlRpcServer();
-		
-		
-	}
+
+    public static void main(String[] args) throws Exception {
+
+        new DCXmlRpcServer();
+
+
+    }
 
 }
